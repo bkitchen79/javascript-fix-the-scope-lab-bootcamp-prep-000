bcc6518e5b675c52d56cb7f662ddcb04d7baf811
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-08 04:34:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ function myAnimal() {
   return animal
 }
 
+myAnimal()
+
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+
+your
 
 function add2(n) {
   const two = 2
